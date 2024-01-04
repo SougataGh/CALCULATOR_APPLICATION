8bac889601cbe25e05980f3102109132524ebcdc
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-04 19:17:36 +0530

    Create Advance_Calculator.h

---
+++ Advance_Calculator.h
@@ -0,0 +1,15 @@
+class Advance_Calculator : public Calculator {
+protected:
+    void add();
+    void substract();
+    void multiply();
+    void division();
+    void modulo();
+    void power();
+    void squareRoot();
+    void logr();
+    void show_History();
+
+public:
+    void choose_from_Advcalc();
+};
