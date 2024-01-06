209b5713259721bd36602ba8e90607ff1a2d65ad
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-06 17:40:54 +0530

    Update check_Validity.cpp

--- check_Validity.cpp
+++ check_Validity.cpp
@@ -1,13 +1,14 @@
 void check_Validity::Check(double &x){
+	                //loops until sucessful extraction
 			while (!(cin >> x))
-           {
+                     {
 	             cout << "INVALID INPUT. PLEASE ENTER COORECT NUMBERS........\n";
 	
 	            // Clear the fail state and ignore the invalid input in the buffer
 			        cin.clear();
 			        cin.ignore();
-            }
-		}
+                     }
+  }
 void check_Validity::Check(double &x,double &y)
 {
 	while (!(cin >> x>>y))
