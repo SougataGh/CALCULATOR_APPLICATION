9e3140158c813a5400af5483e819db4837acefed
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-06 17:20:14 +0530

    Create check_Validity.cpp

---
+++ check_Validity.cpp
@@ -0,0 +1,21 @@
+void check_Validity::Check(int &x){
+			while (!(cin >> x))
+           {
+	             cout << "Invalid input. Please enter correct numbers.\n";
+	
+	            // Clear the fail state and ignore the invalid input in the buffer
+			        cin.clear();
+			        cin.ignore();
+            }
+		}
+void check_Validity::Check(double &x,double &y)
+{
+	while (!(cin >> x>>y))
+    {
+	    cout << "Invalid input. Please enter correct numbers.\n";
+	
+	            // Clear the fail state and ignore the invalid input in the buffer
+		 cin.clear();
+	     cin.ignore();
+    }
+}
